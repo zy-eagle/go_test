46a90474c79998b18b08e531df6f4856db75647e
zhaoyi3 <zhaoyi3@kingsoft.com>
2025-07-25 15:06:38 +0800

    add demo

--- testCommon/loop_test.go
+++ testCommon/loop_test.go
@@ -24,6 +24,43 @@ loop:
 
 }
 
+func TestGoto(t *testing.T) {
+	fmt.Println("===================1")
+
+loop:
+	for j := 0; j < 3; j++ {
+		fmt.Println("j=", j)
+		for i := 0; i < 10; i++ {
+			if i == 3 {
+				goto loop // goto会跳转到指定标签处，直接进入下一次循环
+			}
+			time.Sleep(time.Second)
+		}
+	}
+
+	fmt.Println("===================2")
+
+}
+
+func TestContinue(t *testing.T) {
+	fmt.Println("===================1")
+
+loop:
+	for j := 0; j < 3; j++ {
+		fmt.Println("j=", j)
+		for i := 0; i < 10; i++ {
+			if i == 3 {
+				continue loop // continue会跳过本次循环，直接进入下一次循环
+			}
+			time.Sleep(time.Second)
+		}
+
+	}
+
+	fmt.Println("===================2")
+
+}
+
 func TestAnd(t *testing.T) {
 	fmt.Println(0100 & 0001)
 }
