970e016b0a3494c708462e53d29563c5a9670fad
zhaoyi3 <zhaoyi3@kingsoft.com>
2025-09-02 08:50:51 +0800

    add demo

--- testInterface/interface_test.go
+++ testInterface/interface_test.go
@@ -1,9 +1,59 @@
 package interface_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 	"testing"
 )
+
+type NilS struct {
+	name string
+}
+
+func TestUnmarsh(t *testing.T) {
+	data := struct {
+		Name   string `json:"name"`
+		UserId int64  `json:"user_id"`
+		Age    int    `json:"age"`
+	}{
+		Name:   "test",
+		UserId: 123456789,
+		Age:    18,
+	}
+
+	b, _ := json.Marshal(data)
+
+	m := make(map[string]interface{})
+
+	_ = json.Unmarshal(b, &m)
+
+	fmt.Println(m)
+
+	m1 := make(map[string]interface{})
+	dec := json.NewDecoder(bytes.NewReader(b))
+	dec.UseNumber()
+	if err := dec.Decode(&m1); err != nil {
+		fmt.Println("decode err ", err)
+		return
+	}
+	fmt.Println(m1["user_id"])
+}
+
+func TestNil(t *testing.T) {
+	var ns *NilS = nil
+
+	println(IfElse(ns != nil, ns.name))
+}
+
+func IfElse(ifv bool, rl string) string {
+	if ifv {
+		return rl
+	}
+
+	return ""
+}
 
 func TestInterface(t *testing.T) {
 	var invoker Invoker
--- testSplit/split_test.go
+++ testSplit/split_test.go
@@ -1,33 +1,45 @@
 package testSplit
 
 import (
+	"encoding/json"
 	"fmt"
-	"regexp"
-	"strings"
 	"testing"
 )
 
 func TestSplit(t *testing.T) {
-	str1 := "schema.canal.*"
-	fmt.Println(strings.SplitN(str1, ".", 2))
+	//str1 := "schema.canal.*"
+	//fmt.Println(strings.SplitN(str1, ".", 2))
+	//
+	//str2 := "schema"
+	//fmt.Println(strings.SplitN(str2, ".", 2))
+	//
+	//str3 := `13d80f1d-566c-11f0-baae-fa163e7e863f:1-2,\\n13dc7c32-566c-11f0-bb08-fa163eed320f:1-8,\\\n13dc7c32-566c-11f0-bb08-fa163eed320f:1-8\n`
+	//
+	//parts := strings.Split(str3, ",")
+	//re := regexp.MustCompile(`^(\\\\+n|\\n)|(\\\\+n|\\n)$`)
+	//res := make([]string, 0, len(parts))
+	//for _, part := range parts {
+	//	temp := re.ReplaceAllString(part, "")
+	//	res = append(res, temp)
+	//}
+	//
+	//fmt.Println(strings.Join(res, ","))
 
-	str2 := "schema"
-	fmt.Println(strings.SplitN(str2, ".", 2))
-
-	str3 := `13d80f1d-566c-11f0-baae-fa163e7e863f:1-2,\\n13dc7c32-566c-11f0-bb08-fa163eed320f:1-8,\\\n13dc7c32-566c-11f0-bb08-fa163eed320f:1-8\n`
-
-	parts := strings.Split(str3, ",")
-	re := regexp.MustCompile(`^(\\\\+n|\\n)|(\\\\+n|\\n)$`)
-	res := make([]string, 0, len(parts))
-	for _, part := range parts {
-		temp := re.ReplaceAllString(part, "")
-		res = append(res, temp)
-	}
-
-	fmt.Println(strings.Join(res, ","))
+	str4 := "ddd"
+	fmt.Println(str4[:2])
 
 	//tempGTID := strings.Replace(strings.TrimSpace(str3), "\\\\n", "", -1)
 	//fmt.Println(tempGTID)
 	//tempGTID1 := strings.Replace(tempGTID, "\\n", "", -1)
 	//fmt.Println(tempGTID1)
+
+	m := map[string]interface{}{
+		"name": "eagle",
+		"age":  18,
+	}
+
+	res, _ := json.Marshal(m)
+
+	fmt.Println(string(res))
+
 }
